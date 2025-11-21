b78a917a46c4fe4087e65815a6d15f555f2ec5a7
Stanislau <stas.rogal89@gmail.com>
2025-11-21 20:20:55 +0300

    Active button, animation of best_offer, logic

--- languages/mainJS.js
+++ languages/mainJS.js
@@ -17,7 +17,7 @@ function changeLanguage() {
     if (hash) { // Проверяем, есть ли хэш
         hash = hash.substr(1); // Убираем символ '#'
         console.log("хэш - ", hash);
-        
+
         // Устанавливаем значение select по хэшу, если он валиден
         if (allLang.includes(hash)) {
             select.value = hash;
@@ -45,3 +45,50 @@ function changeLanguage() {
 }
 
 changeLanguage();
+
+document.addEventListener('DOMContentLoaded', function () {
+    const yearlyBtn = document.getElementById('yearlyAccess');
+    const weeklyBtn = document.getElementById('weeklyAccess');
+    const continueBtn = document.getElementById('continue-btn');
+    const bestOffer = document.getElementById('best_offer');
+
+    // Обработчики событий для кнопок подписки
+    yearlyBtn.addEventListener('click', function () {
+        setActiveButton('yearly');
+    });
+
+    weeklyBtn.addEventListener('click', function () {
+        setActiveButton('weekly');
+    });
+
+    // Обработчик для кнопки Continue
+    continueBtn.addEventListener('click', function () {
+        if (yearlyBtn.classList.contains('active')) {
+            // Переход по ссылке для годовой подписки
+            window.location.href = 'https://apple.com/';
+            return; // Предотвращает выполнение дальнейшего кода
+        } else if (weeklyBtn.classList.contains('active')) {
+            // Переход по ссылке для недельной подписки
+            window.location.href = 'https://google.com/';
+            return; // Предотвращает выполнение дальнейшего кода
+        } else {
+            continueBtn.classList.add('negative');
+            setTimeout(() => {
+                continueBtn.classList.remove('negative'); // Удаляем класс после завершения анимации
+            }, 500);
+        }
+    });
+
+    // Функция для установки активной кнопки
+    function setActiveButton(type) {
+        if (type === 'yearly') {
+            bestOffer.classList.remove('unselect');
+            yearlyBtn.classList.add('active');
+            weeklyBtn.classList.remove('active');
+        } else {
+            weeklyBtn.classList.add('active');
+            yearlyBtn.classList.remove('active');
+            bestOffer.classList.add('unselect');
+        }
+    }
+}); 
